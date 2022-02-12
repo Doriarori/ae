9a122c7d76752c0bb5b36fe12d492e8e5cdce735
Doriarori <skigin02@mail.ru>
2022-02-12 13:56:59 +0300

    second

--- ches.py
+++ ches.py
@@ -1,1 +1,25 @@
-s
+data = ''
+with open('data.txt', 'r', encoding='utf-8') as f:
+	for line in f:
+		data = data + line
+
+print(data)
+s = 'hello'
+res = []
+
+hot = input('Hot: ')
+cold = input('Cold: ')		
+
+
+
+res = res[:-1]
+print(res)
+
+data += '\n'
+data += 'Hot water: ' + hot + '\n'
+data += 'Cold water: ' + cold + '\n'
+
+print(data)
+
+with open('data.txt', 'w', encoding='utf-8') as f:
+	f.write(data)
